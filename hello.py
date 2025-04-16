6f8cc1d8ac62747647e16cc10c9d5253d73a7863
skyfila <95835376+skyfila@users.noreply.github.com>
2025-04-16 19:25:21 -0400

    Update hello.py
    added new greeting

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 # Kensea Jordan
 # 4/16/2025
-# Asimple python program to print a message
+# A simple python program to print a message
+
+# Kensea Jordan
+# 4/16/2025
+# added new message
 
 print("Hello, World!")
+
+print("Welcome to GitHub!")
